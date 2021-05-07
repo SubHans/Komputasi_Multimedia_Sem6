a04fea10dc1dc8a9915664b8366f23de6a7be10a
SubHans <56150593+SubHans@users.noreply.github.com>
2021-05-07 16:29:32 +0800

    Minggu 9 (UTS)

---
+++ Minggu uts/Uts Komputasi Multimedia/Assets/Script/MenuAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAction : MonoBehaviour
+{
+    public void MENU_ACTION_GotoPage(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Minggu uts/Uts Komputasi Multimedia/Assets/Script/PlayerInventoryDisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PlayerInventoryDisplay : MonoBehaviour
+{
+    public Image[] shuttlePlaceholders;
+    public Sprite iconShuttleColour;
+    public Sprite iconShuttleGrey;
+    public void OnChangeShuttleTotal(int ShuttleTotal)
+    {
+        for (int i = 0; i < shuttlePlaceholders.Length; ++i)
+        {
+            if (i < ShuttleTotal)
+                shuttlePlaceholders[i].sprite = iconShuttleColour;
+            else
+                shuttlePlaceholders[i].sprite = iconShuttleGrey;
+        }
+    }
+}
